3afb90e63cac8d479eb98b043bfbdd602f41a071
Maxime Moreillon <m.moreillon@gmail.com>
2020-07-11 11:37:25 +0000

    fixing dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ for file in $ROOT_DIR/js/app.*.js* $ROOT_DIR/index.html $ROOT_DIR/precache-manif
 do
   echo "Processing $file ...";
 
-  sed -i 's|VUE_APP_FINANCES_API_URL|'${VUE_APP_FINANCES_API_URL}'|g' $file
+  #sed -i 's|VUE_APP_FINANCES_API_URL|'${VUE_APP_FINANCES_API_URL}'|g' $file
   sed -i 's|VUE_APP_AUTHENTICATION_API_URL|'${VUE_APP_AUTHENTICATION_API_URL}'|g' $file
   sed -i 's|VUE_APP_AUTHENTICATION_FRONT_URL|'${VUE_APP_AUTHENTICATION_FRONT_URL}'|g' $file
 
