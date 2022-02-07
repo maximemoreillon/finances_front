259c1f701b5e384410f8f3e9d68d43e8ce2a1a02
Maxime Moreillon <m.moreillon@gmail.com>
2022-02-07 11:19:46 +0000

    env

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,8 @@ for file in $ROOT_DIR/js/*.js* $ROOT_DIR/index.html $ROOT_DIR/precache-manifest*
 do
   echo "Processing $file ...";
 
-  sed -i 's|VUE_APP_API_URL_PLACEHOLDER|'${VUE_APP_API_URL}'|g' $file
+  sed -i 's|VUE_APP_FINANCES_API_URL_PLACEHOLDER|'${VUE_APP_FINANCES_API_URL}'|g' $file
+  sed -i 's|VUE_APP_AUTHENTICATION_API_URL_PLACEHOLDER|'${VUE_APP_AUTHENTICATION_API_URL}'|g' $file
 
 done
 
