f253832fd4d1a4c00a3fabb2c7689e0297e4c5a4
Maxime Moreillon <m.moreillon@gmail.com>
2023-09-22 07:24:48 +0900

    started AccountPreview

---
+++ src/components/AccountPreview.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card :loading="loading">
+    <v-card-title> {{ account }} </v-card-title>
+    <v-card-text> </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "AccountPreview",
+  props: {
+    account: String,
+  },
+  data() {
+    return {
+      balance: 0,
+      currency: "",
+      loading: false,
+    }
+  },
+}
+</script>
