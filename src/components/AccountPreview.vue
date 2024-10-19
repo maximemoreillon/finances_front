684d49dbe8616e15be37619dc234d9b568f2efba
Maxime Moreillon <m.moreillon@gmail.com>
2024-10-19 18:21:14 +0900

    account preview

--- src/components/AccountPreview.vue
+++ src/components/AccountPreview.vue
@@ -5,18 +5,18 @@
     :to="{ name: 'account', params: { accountId: account.id } }"
   >
     <v-card-title> {{ account.name }} </v-card-title>
-    <v-card-subtitle> Currency: {{ account.currency }} </v-card-subtitle>
-    <!-- <v-card-text>
+    <!-- <v-card-subtitle> Currency: {{ account.currency }} </v-card-subtitle> -->
+    <v-card-text>
       <v-row dense>
         <v-col> Currency: {{ account.currency }} </v-col>
       </v-row>
-      <v-row dense>
-        <v-col> Balance: {{ account.balance }} </v-col>
+      <v-row dense v-if="account.balance">
+        <v-col> Balance: {{ Math.round(account.balance * 100) / 100 }} </v-col>
       </v-row>
-      <v-row dense>
+      <v-row dense v-if="account.transaction_count">
         <v-col> Transactions: {{ account.transaction_count }} </v-col>
-      </v-row> 
-    </v-card-text>-->
+      </v-row>
+    </v-card-text>
   </v-card>
 </template>
 
