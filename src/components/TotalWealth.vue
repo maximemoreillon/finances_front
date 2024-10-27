43824e415a8c2f6e6726dee2a71eb6d866d44279
Maxime Moreillon <m.moreillon@gmail.com>
2024-10-27 12:19:44 +0900

    summary

--- src/components/TotalWealth.vue
+++ src/components/TotalWealth.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>
-    <template v-if="!loading && total">
-      Total assets: {{ currency }} {{ parseFloat(total).toLocaleString() }}
-    </template>
-    <v-progress-circular indeterminate v-if="loading" />
-  </div>
+  <v-card outlined>
+    <v-card-title> Summary </v-card-title>
+    <v-card-text>
+      <v-row v-if="loading">
+        <v-col>
+          <v-progress-linear indeterminate />
+        </v-col>
+      </v-row>
+
+      <v-row v-else align="center">
+        <v-col>
+          Total assets: {{ currency }} {{ parseFloat(total).toLocaleString() }}
+        </v-col>
+        <v-col cols="3">
+          <v-select :items="currencies" v-model="currency" label="Currency" />
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
@@ -12,40 +25,52 @@ export default {
   name: "TotalWealth",
   data() {
     return {
-      total: null,
       loading: false,
       rates: null,
       currency: "JPY",
+      accounts: [],
     }
   },
   async mounted() {
     try {
       this.loading = true
       await this.getExchangeRate()
-      await this.getTotal()
+      await this.getAccounts()
     } catch (error) {
       console.error(error)
     } finally {
       this.loading = false
     }
   },
+  watch: {
+    currency() {
+      this.getExchangeRate()
+    },
+  },
   methods: {
     async getExchangeRate() {
       const url = `/rate`
-      const params = { currency: "JPY" }
+      const params = { currency: this.currency }
       const { data } = await this.axios.get(url, { params })
       this.rates = data.rates
     },
-    async getTotal() {
+    async getAccounts() {
       const url = `/accounts`
       const { data } = await this.axios.get(url)
-      this.total = data.accounts.reduce((prev, account) => {
+      this.accounts = data.accounts
+    },
+  },
+  computed: {
+    total() {
+      return this.accounts.reduce((prev, account) => {
         const { currency, balance } = account
         prev += balance / this.rates[currency]
         return prev
       }, 0)
     },
+    currencies() {
+      return this.accounts.map((a) => a.currency)
+    },
   },
-  computed: {},
 }
 </script>
