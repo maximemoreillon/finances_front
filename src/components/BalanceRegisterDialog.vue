641bfc9bfd6a800b7e2ffde02b29e7c921f887ad
Maxime Moreillon <m.moreillon@gmail.com>
2026-06-13 14:39:32 +0900

    balance validation

--- src/components/BalanceRegisterDialog.vue
+++ src/components/BalanceRegisterDialog.vue
@@ -8,14 +8,14 @@
 
     <v-card>
       <v-card-title>Register balance</v-card-title>
-      <v-form @submit.prevent="registerBalance">
+      <v-form @submit.prevent="registerBalance" ref="form">
         <v-card-text>
           <v-row justify="center">
             <v-col>
               <v-text-field
-                type="number"
-                v-model.number="balance"
+                v-model="balance"
                 label="Balance"
+                :rules="[validateNumberWithCommas]"
                 :prefix="currency"
               />
             </v-col>
@@ -25,7 +25,9 @@
               <v-btn @click="dialog = false" variant="text">Cancel</v-btn>
             </v-col>
             <v-col cols="auto">
-              <v-btn type="submit" :loading="registering" color="primary">Save</v-btn>
+              <v-btn type="submit" :loading="registering" color="primary"
+                >Save</v-btn
+              >
             </v-col>
           </v-row>
         </v-card-text>
@@ -35,29 +37,52 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue"
-import axios from "@/axios"
+import { ref } from "vue";
+import axios from "@/axios";
+import z from "zod";
 
-const props = defineProps<{ accountId: string; currency?: string }>()
-const emit = defineEmits<{ balanceRegistered: [data: unknown] }>()
+const props = defineProps<{ accountId: string; currency?: string }>();
+const emit = defineEmits<{ balanceRegistered: [data: unknown] }>();
 
-const dialog = ref(false)
-const registering = ref(false)
-const balance = ref(0)
+const dialog = ref(false);
+const registering = ref(false);
+const balance = ref(0);
+const form = ref();
+
+const cleanNumberSchema = z
+  .union([
+    z.number(),
+    z.string().transform((str) => Number(str.replace(/,/g, ""))),
+  ])
+  .pipe(z.number());
+
+const validateNumberWithCommas = (value: any) => {
+  if (value === null || value === undefined || value === "") return true;
+
+  const result = cleanNumberSchema.safeParse(value);
+
+  if (result.success) return true;
+  else {
+    return "Malformed number";
+  }
+};
 
 async function registerBalance() {
-  registering.value = true
+  registering.value = true;
+
   try {
+    const cleanedBalance = cleanNumberSchema.parse(balance.value);
+
     const { data } = await axios.post(`/accounts/${props.accountId}/balance/`, {
-      balance: balance.value,
-    })
-    emit("balanceRegistered", data)
-    dialog.value = false
+      balance: cleanedBalance,
+    });
+    emit("balanceRegistered", data);
+    dialog.value = false;
   } catch (error) {
-    console.error(error)
-    alert("Error")
+    console.error(error);
+    alert("Error");
   } finally {
-    registering.value = false
+    registering.value = false;
   }
 }
 </script>
